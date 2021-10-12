ddb8eef4b649f28e466345357b98ff0d0e538e9f
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-10-12 14:11:02 +0200

    created scss files

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -36,11 +36,9 @@ export class MovieView extends React.Component {
 
 MovieView.propTypes = {
     movie: PropTypes.shape({
+      ImagePath: PropTypes.string.isRequired,
       Title: PropTypes.string.isRequired,
       Description: PropTypes.string.isRequired,
-      Genre: PropTypes.string.isRequired,
-      Director: PropTypes.string.isRequired,
-      ImagePath: PropTypes.string.isRequired
     }).isRequired,
     onBackClick: PropTypes.func.isRequired
   };
