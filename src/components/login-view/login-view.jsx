93d9d48b7457c9dfeb24681c32e0022c598959d1
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-10-14 16:05:50 +0200

    added bootstrap container

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -14,7 +14,7 @@ export function LoginView(props) {
     console.log(username, password);
     /* Send a request to the server for authentication */
     /* then call props.onLoggedIn(username) */
-    props.onLoggedIn(username);
+    props.onLoggedIn(user);
   };
   
   return (
