c3925477ba4cfbfba99a23885da68aea770c6b43
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-10-14 16:31:40 +0200

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
 export class MovieView extends React.Component {
 
     render() {
@@ -8,22 +9,36 @@ export class MovieView extends React.Component {
 
         return (
             <div className="movie-view">
-                <div className="movie-poster">
-                    <img src={movie.ImagePath} />
-                </div>
-                <div className="movie-title">
-                    <span className="label">Title: </span>
-                    <span className="value">{movie.Title}</span>
-                </div>
-                <div className="movie-description">
-                    <span className="label">Description: </span>
-                    <span className="value">{movie.Description}</span>
-                </div>
-                <button onClick={() => { onBackClick(null); }}>Back</button>
-
+              <div className="movie-poster">
+                <img src={movie.ImagePath} />
+              </div>
+              <div className="movie-title">
+                <span className="label"> Title: </span>
+                <span className="value">{movie.Title}</span>
+              </div>
+              <div className="movie-description">
+                <span className="label">Description: </span>
+                <span className="value">{movie.Description}</span>
+              </div>
+              <div className="movie-genre">
+                <span className="label">Genre: </span>
+                <span className="value">{movie.Genre.Name}</span>
+              </div>
+              <div className="movie-director">
+                <span className="label">Director: </span>
+                <span className="value">{movie.Director.Name}</span>
+              </div>
+              <button
+                onClick={() => {
+                  onBackClick(null);
+                }}
+              >
+                Back
+              </button>
             </div>
-        );
-    }
+          );
+        }
+      }
 }
 
 MovieView.propTypes = {
