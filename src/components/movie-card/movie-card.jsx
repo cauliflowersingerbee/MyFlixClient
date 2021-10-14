0050c5b0fbe136611425b073b017ab28f9ff456d
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-10-14 16:40:52 +0200

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row'
 
 
 export class MovieCard extends React.Component {
@@ -10,17 +7,17 @@ export class MovieCard extends React.Component {
       const { movie, onMovieClick } = this.props;
   
       return (
-          <Card>
-            <Card.Img variant="top" src={movie.ImagePath} />
-            <Card.Body>
-              <Card.Title>{movie.Title}</Card.Title>
-              <Card.Text>{movie.Description}</Card.Text>
-              <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
-            </Card.Body>
-          </Card>
-        );
-      }
+        <div
+          className="movie-card"
+          onClick={() => {
+            onMovieClick(movie);
+          }}
+        >
+          {movie.Title}
+        </div>
+      );
     }
+  }
   
   MovieCard.propTypes = {
     movie: PropTypes.shape({
