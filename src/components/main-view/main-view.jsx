54244f8060e5cae2dde48175d2e38ef0adb826b7
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-10-14 16:48:57 +0200

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,8 +5,8 @@ import { LoginView } from '../login-view/login-view';
 import {RegistrationView} from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+
+
 
 export class MainView extends React.Component {
     constructor(){
@@ -20,7 +20,8 @@ export class MainView extends React.Component {
       }
 
       componentDidMount(){
-        axios.get('https://kino-noir.herokuapp.com/movies')
+        axios
+        .get('https://kino-noir.herokuapp.com/movies')
           .then(response => {
             this.setState({
               movies: response.data
@@ -74,20 +75,28 @@ export class MainView extends React.Component {
 
     return (
       <div className="main-view">
-        {selectedMovie
-          ? (
-            <Row>
-              <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-            </Row>
-          )
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+        {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
+        {selectedMovie ? (
+          <MovieView
+            movie={selectedMovie}
+            onBackClick={(newSelectedMovie) => {
+              this.setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ) : (
+          movies.map((movie) => (
+            <MovieCard
+              key={movie._id}
+              movie={movie}
+              onMovieClick={(newSelectedMovie) => {
+                this.setSelectedMovie(newSelectedMovie);
+              }}
+            />
           ))
-        }
+        )}
       </div>
     );
-    
-  
-
+  }
+}
 
     
