726635e681d45a2baf64e8718b2def13775265b4
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-10-16 13:36:15 +0200

    updated index.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,91 +1,14 @@
 import React from 'react';
-import axios from 'axios';
-
-import { LoginView } from '../login-view/login-view';
-import {RegistrationView} from '../registration-view/registration-view';
-import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../movie-view/movie-view';
-
-
 
 export class MainView extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-          movies: [],
-          selectedMovie: 'null',
-          user: 'null',
-          newUser: 'null',
-        };
-      }
-
-      componentDidMount(){
-        axios
-        .get('https://kino-noir.herokuapp.com/movies')
-        .then(response => {
-          this.setState({
-            movies: response.data
-          });
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
-    /*When a movie is clicked, this function is invoked and 
-    updates the state of the `selectedMovie` *property to 
-    that movie*/
-      
-    setSelectedMovie(movie) {
-      this.setState({
-        selectedMovie: movie
-      });
-    }
-  
-    
-    /* When a user successfully logs in, this function 
-    updates the `user` property in state to that 
-    *particular user*/
-
-  onLoggedIn(user) {
-    this.setState({
-      user
-    });
-  }
-
-  onRegistration(newUser) {
-    this.setState({
-      newUser
-    });
-  }
 
   render() {
-    const { movies, selectedMovie, user, newUser } = this.state;
-      
-    /* If there is no user, the LoginView is rendered. 
-    If there is a user logged in, the user details 
-    are *passed as a prop to the LoginView*/
-
-    if (user === 'null') {return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;}
-
-    else
-
-    if (newUser) return <RegistrationView onRegistration={newUser => this.onRegistration(newUser)} />;
-
-  
-
-    // Before the movies have been loaded
-    if (movies.length === 0) 
-  
     return (
       <div className="main-view">
-        {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
-         ))
-        }
+        <div>Inception</div>
+        <div>The Shawshank Redemption</div>
+        <div>Gladiator</div>
       </div>
     );
   }
 }
-    
