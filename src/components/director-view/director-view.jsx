e539fdf44eed2971b01bc8c85bf17265cd42b30d
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-10-24 16:09:11 +0200

    Update director-view.jsx

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Card, Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+export function DirectorView (props) {
+
+      const { director } = props;
+      console.log(director)
+  
+      return (
+        <>
+            <Row>
+                <Col>
+                    <Card>
+                      <Card.Body>
+                      <div className="director-name">
+                         <span className="label">Name: </span>
+                         <span className="value">{director.Name}</span>
+                      </div>
+                      <div className="director-bio">
+                          <span className="label">Bio: </span>
+                          <span className="value">{director.Bio}</span>
+                      </div>
+                      <Link to={`/`}>
+                        <Button style={{marginTop: '1rem'}} variant="primary">Go back to movie list</Button>
+                      </Link>
+                      </Card.Body>
+                    </Card>
+              </Col>
+           </Row>
+        </>
+      );
+    }
+  
+
