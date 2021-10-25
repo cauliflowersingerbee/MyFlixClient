0af8db5b619673151f481d735d07fd9d1513622e
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-10-25 13:34:20 +0200

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -9,6 +9,34 @@ export function ProfileView(props) {
 //Allow a user to update their user info 
 //(username, password, email, date of birth
 
+componentDidMount() {
+  let accessToken = localStorage.getItem('token');
+  if (accessToken !== null) {
+    this.setState({
+      user: localStorage.getItem('user')
+    });
+    this.getUsers(accessToken);
+  }
+}
+
+getUsers(token) {
+  axios.get(`https://kino-noir.herokuapp.com/users/${username}`, {
+    headers: { Authorization: `Bearer ${token}`}
+  })
+  .then(response => {
+    //result becomes state
+    this.setState({
+          Name: response.data.Name,
+          Username: response.data.Username,
+          Password: response.data.Password,
+          Email: response.data.Email,
+          Birthdate: response.data.Birthdate,
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
 
 
 //Allow a user to deregister
