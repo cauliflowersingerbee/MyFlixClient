d0b94d285a83b1ab141cb1d31bb549abff271ea3
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-10-25 13:53:31 +0200

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -9,36 +9,61 @@ export function ProfileView(props) {
 //Allow a user to update their user info 
 //(username, password, email, date of birth
 
-componentDidMount() {
-  let accessToken = localStorage.getItem('token');
-  if (accessToken !== null) {
-    this.setState({
-      user: localStorage.getItem('user')
-    });
-    this.getUsers(accessToken);
-  }
+
+
+function updateUser ({ handleSubmit, handleUpdate}) {
+   return  (
+    <Container className="registration-card">  
+    <Row>
+      <Col>
+            <Card style={{ width: '20rem', marginTop: '5rem', marginBottom: '1rem', height: '28rem'}} xs={2}>
+              <Card.Body>
+                <Card.Title>Please Update Your User Details</Card.Title>
+                  <Form>
+                    <Form.Group controlId="formUsername">
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control type="text" onChange={e => handleUpdate(e)} required
+                      placeholder="Please enter a username"/>
+                    </Form.Group>
+
+                    <Form.Group controlId="formPassword">
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control type="password" onChange={e => handleUpdate(e)} required minLength="6"
+                      placeholder="Please enter a password"/>
+                    </Form.Group>
+
+                    <Form.Group controlId="formEmail">
+                    <Form.Label>Email:</Form.Label>
+                    <Form.Control type="email" onChange={e => handleUpdate(e)} required
+                      placeholder="Please enter an email address"/>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBirthday">
+                    <Form.Label>Birthday:</Form.Label>
+                    <Form.Control type="birthday" onChange={e => handleUpdate(e)} required
+                      placeholder="Please enter your birthday"/>
+                    </Form.Group>
+
+                    <Button style={{marginTop: '1rem', }} variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+                    
+                    <div>
+                      <img src={logo} alt="Kino Noir Logo" style={{height: '4rem', width: '7rem', marginTop: '0.1rem'}}/>
+                    </div> 
+                          
+                </Form>  
+            </Card.Body>
+          </Card>
+      </Col>
+      <Col xs={7}>
+        <div className="float-right"> 
+            <img src={img} alt="Cool woman wearing sunglasses leaning back against boat" style={{height: '100%', width: '100%', marginRight: '2rem'}}/>
+        </div>
+      </Col>
+    </Row>
+    </Container>
+   );
+
 }
-
-getUsers(token) {
-  axios.get(`https://kino-noir.herokuapp.com/users/${username}`, {
-    headers: { Authorization: `Bearer ${token}`}
-  })
-  .then(response => {
-    //result becomes state
-    this.setState({
-          Name: response.data.Name,
-          Username: response.data.Username,
-          Password: response.data.Password,
-          Email: response.data.Email,
-          Birthdate: response.data.Birthdate,
-    });
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
-
-
 //Allow a user to deregister
 
 
