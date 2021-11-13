bb1f5710ab5285c9b79675c0a70a7393d18068fa
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-11-13 13:46:07 +0100

    created search button component

---
+++ src/components/search-button-view/search-button.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import PropTypes from "prop-types";
+import { Card, Form, Button, Container, Row, Col } from 'react-bootstrap';
+import img from '../../img/LoginImg.jpg';
+import logo from '../../img/KinoNoirLogo.png';
+import axios from 'axios';
+import { Link } from "react-router-dom";
+
+
+export class SearchButtonView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        
+    }
+  }
+
+  
