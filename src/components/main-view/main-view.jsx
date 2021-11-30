651adad6700ead0357019461b4b885512ae9ee38
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-11-30 11:41:17 +0100

    updated files

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -89,9 +89,9 @@ class MainView extends React.Component {
 
   onLoggedIn(authData) {
     console.log(authData);
-    this.setState({
-      user: authData.user.Username
-    });
+    //this.setState({
+      //user: authData.user.Username
+    //});
   
     localStorage.setItem('token', authData.token);
     localStorage.setItem('user', authData.user.Username);
